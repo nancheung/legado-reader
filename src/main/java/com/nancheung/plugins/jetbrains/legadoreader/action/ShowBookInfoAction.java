91c15f561b3823aa93c7af26dcac750fe88efa99
NanCheung <i@nancheung.com>
2023-09-27 17:14:37 +0800

    :zap: perf: 降低更新章节标题的频率

--- src/main/java/com/nancheung/plugins/jetbrains/legadoreader/action/ShowBookInfoAction.java
+++ src/main/java/com/nancheung/plugins/jetbrains/legadoreader/action/ShowBookInfoAction.java
@@ -7,22 +7,40 @@ import com.nancheung.plugins.jetbrains.legadoreader.api.dto.BookDTO;
 import com.nancheung.plugins.jetbrains.legadoreader.dao.CurrentReadData;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * 自动更新显示当前阅读的书籍信息
+ *
+ * @author NanCheung
+ */
 public class ShowBookInfoAction extends AnAction {
-    
+
+    private String bookName = "";
+    private String chapterTitle = "";
+
     @Override
     public void actionPerformed(AnActionEvent e) {
     }
-    
+
     @Override
     public void update(@NotNull AnActionEvent e) {
         BookDTO book = CurrentReadData.getBook();
-        
+
         if (book == null || CollUtil.isEmpty(CurrentReadData.getBookChapterList())) {
             return;
         }
-        
+
+        String name = book.getName();
+        String title = CurrentReadData.getBookChapter().getTitle();
+
+        if (bookName.equals(name) && chapterTitle.equals(title)) {
+            return;
+        }
+
+        bookName = name;
+        chapterTitle = title;
+
         // 将阅读信息设置到action的描述中
-        String text = book.getName() + " - " + CurrentReadData.getBookChapter().getTitle();
+        String text = name + " - " + title;
         e.getPresentation().setText(text);
     }
 }
